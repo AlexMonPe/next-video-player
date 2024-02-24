0964b7fb13ea2365d3e258ac268d52d82c368d60
Alex Montero <alex_bcn10@hotmail.es>
2024-02-24 17:37:48 +0100

    Create incrementPlayCount endpoint and change hardcoded video data in getVideos endpoint to get all videos from DB. Add video types with zod in trpc server.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,20 +1,71 @@
+import Database from 'better-sqlite3';
 import { publicProcedure, router } from './trpc';
 
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+
+import { z } from 'zod';
+import { videos } from '@/db/schema';
+import { seed } from '@/db/seed';
+import { eq } from 'drizzle-orm';
+import { TRPCError } from '@trpc/server';
+
+const sqlite = new Database('sqlite.db');
+const db = drizzle(sqlite);
+
+migrate(db, { migrationsFolder: 'drizzle' });
+
+const videoSchema = z.object({
+  id: z.number(),
+  title: z.string().nullable(),
+  playcount: z.number().nullable(),
+  url: z.string().nullable(),
+  description: z.string().nullable(),
+});
+const videosSchema = z.array(videoSchema);
+await seed().catch((err) => {
+  console.error('Error al poblar la base de datos:', err);
+});
+
 export const appRouter = router({
-  getVideos: publicProcedure.query(async () => {
-    return [
-      {
-        url: 'https://stream.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/high.mp4',
-        title: 'Video 1',
-        description: 'Descripción del video 1',
-      },
-      {
-        url: 'https://stream.mux.com/A3VXy02VoUinw01pwyomEO3bHnG4P32xzV7u1j1FSzjNg/high.mp4',
-        title: 'Video 2',
-        description: 'Descripción del video 2',
-      },
-    ];
+  getVideos: publicProcedure.output(videosSchema).query(async () => {
+    try {
+      return await db.select().from(videos).all();
+    } catch (error) {
+      console.error('Error getting videos:', error);
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'An unexpected error occurred, please try again later.' ,
+        cause: error
+      });
+    }
   }),
+  incrementPlayCount: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        playcount: z.number(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      try {
+        const { id, playcount } = input;
+        const updatedVideo = await db
+          .update(videos)
+          .set({ playcount })
+          .where(eq(videos.id, id))
+          .returning({ updatedVideo: videos });
+
+        return updatedVideo;
+      } catch (error) {
+        console.error('Failed to update playcount:', error);
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'An unexpected error occurred, please try again later.' ,
+          cause: error
+        });
+      }
+    }),
 });
 
 export type AppRouter = typeof appRouter;
