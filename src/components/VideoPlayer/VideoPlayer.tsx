0e0d5d21a5dbdbcc32590cba66a1ac2049c43d9f
Alex Montero <alex_bcn10@hotmail.es>
2024-02-24 17:41:09 +0100

    Create single videoPlayer component

---
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -0,0 +1,38 @@
+import React, { Dispatch, SetStateAction, forwardRef } from 'react';
+
+interface VideoPlayerProps {
+  src: string;
+  id: number;
+  incrementPlayCount?: (videoId: number, currentPlayCount: number, setPlaycount: any) => void;
+  playcount?: number;
+  setPlaycount?: Dispatch<SetStateAction<number | null>>
+}
+
+const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
+  ({ id, src, incrementPlayCount, playcount, setPlaycount }, ref) => {
+
+    //Increment play count via useIncrementPlayCount
+    const handlePlay = () => {
+      if (incrementPlayCount) {
+        incrementPlayCount(id, playcount ?? 1, setPlaycount);
+      }
+    };
+
+    return (
+      <video
+        slot='media'
+        preload='auto'
+        ref={ref}
+        src={src}
+        loop
+        muted
+        className='h-full w-full'
+        onPlay={handlePlay}
+      ></video>
+    );
+  }
+);
+
+VideoPlayer.displayName = 'VideoPlayer';
+
+export default VideoPlayer;
