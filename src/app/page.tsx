4ae8f7bafa8bd3744e56f5a557841517628d5302
Alex Montero <alex_bcn10@hotmail.es>
2024-02-24 17:32:31 +0100

    Delete initial data in home and add cover and videos section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,34 @@
+import Loading from '@/components/Loading/Loading';
 import Videos from '../components/VideoPlayer/Videos';
+import { serverClient } from './_trpc/serverClient';
+import { Suspense } from 'react';
+import Cover from '@/components/Cover/Cover';
+import { Metadata } from 'next';
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: 'Vidext | Home',
+
+};
+export default async function Home() {
+  const videos = await serverClient.getVideos();
+
   return (
-    <main className='w-screen h-full bg-[#F5FAF4]'>
-      <div
-        style={{
-          backgroundImage: `url(/images/backgrounds/lake-bg.webp)`,
-          backgroundPosition: 'center center',
-        }}
-        className='min-h-[45rem] h-[45rem] w-screen bg-cover bg-no-repeat relative flex justify-center items-center'
-      >
-        <div className='absolute text-black inset-0 w-full h-full bg-[#0000003b] z-10'></div>
-        <div className='container mx-auto text-white w-full h-full z-20 flex justify-center items-end pb-14 '>
-          <h1 className='text-5xl drop-shadow-[0px_3px_2px_rgba(0,_0,_0,_0.45)] leading-[3.2rem]'>
-            <strong>Find a new way</strong> to enjoy nature with{' '}
-            <strong>Vidext</strong>
-          </h1>
-        </div>
-      </div>
-      <div className='container mx-auto'>
-        <h2 className='text-2xl drop-shadow-[0px_2px_6px_rgba(0,_0,_0,_0.45)] text-black leading-[3.2rem] my-8'>
-          <strong>Explore</strong> nature videos
+    <main
+      className={`w-screen ${
+        videos?.length ? 'h-full' : 'h-screen'
+      } bg-[#F5FAF4]`}
+    >
+      <Cover />
+      <section className='container mx-auto'>
+        <h2 className='text-2xl drop-shadow-[2px_2px_3px_rgba(0,_0,_0,_0.45)] text-black leading-[3.2rem] my-4 md:my-6'>
+          <strong>Explore</strong> awesome videos
         </h2>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 pb-20'>
-          <Videos />
+        <div>
+          <Suspense fallback={<Loading />} >
+            <Videos videos={videos} />
+          </Suspense>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
