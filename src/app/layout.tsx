60a55a65209200c5a156fe13842c258e1f6b58c3
Alex Montero <alex_bcn10@hotmail.es>
2024-02-24 17:31:35 +0100

    change metadata title of layout, delete useless svgs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,14 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import TrpcProvider from './_trpc/Provider';
-import Link from 'next/link';
-import Image from 'next/image';
 import Header from '@/components/Header/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Vidext Video Player',
+  title: 'Vidext | Video Player',
   description:
-    'Introducing our innovative video player, using latest-generation technology so you can enjoy an immersive experience and immerse yourself in the Vides experience.',
+    'Introducing our innovative video player, using latest-generation technology so you can enjoy an immersive experience and immerse yourself in the landscapes experience.',
 };
 
 export default function RootLayout({
